eb0cf944b3fa86f7ca92affc46f007fa289d978e
Juhodev <jooo.dev@gmail.com>
2020-01-25 20:52:45 +0200

    Layout player stats better

--- popup.js
+++ popup.js
@@ -17,21 +17,28 @@ browser.runtime.onMessage.addListener(message => {
 		const playerData = message[key];
 		const averages = playerData.averages;
 		const nameRow = createDiv('row');
-		addText(nameRow, `Player ${key}`);
-		playerStatsDiv.appendChild(nameRow);
+		addText(nameRow, key, 'player-name');
+		playerDiv.appendChild(nameRow);
 
 		Object.keys(averages).forEach(averageKey => {
 			addText(
 				currentRow,
-				`${averageKey}: ${Math.round(averages[averageKey])}`,
+				`${formatText(averageKey)}: ${Math.round(
+					averages[averageKey],
+				)}`,
 			);
 			console.log('adding text to', currentRow);
 
 			if (currentRow.children.length === 2) {
-				playerStatsDiv.appendChild(currentRow);
+				playerDiv.appendChild(currentRow);
 				currentRow = createDiv('row');
 			}
 		});
+
+		if (currentRow.children.length > 0) {
+			playerDiv.appendChild(currentRow);
+			currentRow = createDiv('row');
+		}
 
 		playerStatsDiv.appendChild(playerDiv);
 
@@ -46,7 +53,7 @@ browser.runtime.onMessage.addListener(message => {
 			);
 		});
 
-		playerStatsDiv.appendChild(killsDiv);
+		playerDiv.appendChild(killsDiv);
 	});
 
 	document.getElementById('container').appendChild(playerStatsDiv);
@@ -58,8 +65,36 @@ function createDiv(name) {
 	return div;
 }
 
-function addText(div, text) {
+function addText(div, text, name) {
 	const span = document.createElement('span');
 	span.textContent = text;
+	if (name !== undefined) {
+		span.className = name;
+	}
 	div.appendChild(span);
+}
+
+function formatText(text) {
+	switch (text) {
+		case 'kills':
+			return 'Kills';
+
+		case 'assists':
+			return 'Assists';
+
+		case 'deaths':
+			return 'Deaths';
+
+		case 'mvps':
+			return 'MVPs';
+
+		case 'ping':
+			return 'Ping';
+
+		case 'hsp':
+			return 'HS%';
+
+		case 'score':
+			return 'Score';
+	}
 }
